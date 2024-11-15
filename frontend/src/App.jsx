7a062a380968cdc5ead077428f2e590b6dde5f6f
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-15 09:57:12 -0500

    React router dom added to project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import {BrowserRouter as Router , Routes, Route, BrowserRouter} from "react-router-dom"
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<Home/>} />
+  <Route path="/signin" element={<SignIn/>} />
+  <Route path="/signup" element={<SignUp/>} />
+  <Route path="/profile" element={<Profile/>} />
+  <Route path="/about" element={<About/>} />
+
+</Routes>
+</BrowserRouter>
   )
 }
 
