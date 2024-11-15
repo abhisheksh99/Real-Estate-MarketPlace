001d367de53947ddf3fa3494ce057d1b969dbc67
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-15 10:25:40 -0500

    Mongo d connected

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from "dotenv"
+import connectDb from './config/Db.js';
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+connectDb()
 
 // Middleware
 app.use(cors({origin:"http://localhost:5173",credentials:true}));
