77721c83258f4fae61cbcb5bf37a51e0a56f376e
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-16 00:17:58 -0500

    login controller added for user auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler"
 import User from "../models/userModel.js"
 import bcrypt from "bcryptjs"
-
+import jwt from "jsonwebtoken"
 
 export const registerUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
@@ -11,17 +11,58 @@ export const registerUser = asyncHandler(async (req, res) => {
         throw new Error("All fields are required");
     }
 
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({ username, email, password: hashedPassword });
 
     await newUser.save();
 
-    res.status(201).json({ message: "User registered successfully" });
+    res.status(201).json({
+        message: "User registered successfully",
+        newUser: {
+            username: newUser.username,
+            email: newUser.email,
+        }
+    });
 });
 
-export const loginUser = asyncHandler(async(req,res)=>{
-})
+export const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Email and password are required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+
+    const isMatchPassword = await bcrypt.compare(password, user.password);
+
+    if (!isMatchPassword) {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: "10d" });
+
+    res.cookie("token", token, { httpOnly: true }).status(200).json({
+        _id: user._id,
+        username: user.username,
+        email: user.email
+    });
+});
 
 export const googleSignIn = asyncHandler(async(req,res)=>{
 })
